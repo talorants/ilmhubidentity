20a02c7540ddeafc3ed3866d97303f069b05d544
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2022-09-20 20:18:18 +0500

    ok

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ilmhub.Identity.Models;
+
+public class LoginViewModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? ReturnUrl { get; set; }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ilmhub.Identity.Models;
+
+public class RegisterViewModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? ReturnUrl { get; set; }
+}
